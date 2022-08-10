5b1ab9f7d5e470f0683c9ee5607610c3b0524eaf
marho onothoja <marho219@gmail.com>
2022-08-10 09:10:02 +0100

    improve jest test suit

--- src/__test__/app.test.ts
+++ src/__test__/app.test.ts
@@ -1,10 +1,20 @@
 import supertest from 'supertest';
 import App from '../app';
+import http_request from '../services/http_request/iqair_request';
 
 const request = supertest(App);
 
+afterEach(() => {
+    // restore the spy created with spyOn
+    jest.restoreAllMocks();
+});
+
+
 it("Gets the test endpoint", async () => {
+    const spy = jest.spyOn(http_request, `getByCoodinates`);
     const res = await request.get("/api/air_quality");
+
+    expect(spy).toHaveBeenCalled();
     expect(res.status).toBe(200);
 });
   
